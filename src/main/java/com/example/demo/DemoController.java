54a1cc519c41c1722f695d3ea2d4304eec8b7e19
edaviles@microsoft.com <edaviles@microsoft.com>
2022-01-26 12:40:23 +0000

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "CI-CD demo about Azure Spring Cloud and GitHub Actions\n";
+    }
+}
